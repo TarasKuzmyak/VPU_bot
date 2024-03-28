b60970af8eead3ed85d1ae9c68f2415f6148655d
Taras Vlad <okuzmak30@gmail.com>
2024-03-28 15:02:53 +0200

    реалізовую простого бота

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 import logging
+import os
+
+
+from dotenv import load_dotenv
+
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+load_dotenv()
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+#list_1 = ('привіт', 'добрий день', 'здоров')
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('telegram_bot_token').build()
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Шо ти {update.effective_user.first_name}')
 
-    application.run_polling()
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = update.message.text.lower()
+
+    if 'привіт' in message:
+        reply_text = f'Привіт {update.effective_user.first_name}!'
+
+    else:
+        reply_text = 'Я тебе не розумію'
+
+    await update.message.reply_text(reply_text)
+
+
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+
+app.run_polling()
+
+
